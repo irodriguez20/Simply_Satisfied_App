dd0d749542922e5b4ac8909d083fe4ac03f99828
India Rodriguez <carnes.s.ia17@gmail.com>
2019-09-09 17:01:27 -0600

    added hamburger menu corrected media queries to hide hamburger menu

---
+++ main.js
@@ -0,0 +1,9 @@
+//main.html hamburger menu
+function toggleClass(){
+    let menu = document.querySelector(".mainMenu");
+    menu.classList.toggle("toggleCls");   
+}
+
+/* let hamburger = document.querySelector(".hamIcon");
+
+hamburger.addEventListener("click", toggleClass); */
