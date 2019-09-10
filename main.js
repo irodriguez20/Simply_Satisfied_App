469333f721674814a273a3562f2c9c181de2aac0
India Rodriguez <carnes.s.ia17@gmail.com>
2019-09-09 20:59:28 -0600

    deleted signup and login pages, won't use until we have learned about databases. Updated home and main html accordingly. Started on main.js.

--- main.js
+++ main.js
@@ -4,6 +4,63 @@ function toggleClass(){
     menu.classList.toggle("toggleCls");   
 }
 
-/* let hamburger = document.querySelector(".hamIcon");
+let apiKey = "cce8b9ce27b94074941076f355a05bae";
+let planUrl = "https://api.spoonacular.com/recipes/mealplans/generate";
+let recipeUrl = "https://api.spoonacular.com/recipes/{id}/information";
 
-hamburger.addEventListener("click", toggleClass); */
+
+function displayResults(responseJson){
+    console.log(responseJson)
+    $("#results-list").empty();
+
+    for( let i=0; i<responseJson.meals.length; i++){
+        let recipe = getRecipe(responseJson.meals[i].id);
+        $("#results-list").append(`
+            '<li><img src='${responseJson.meals[i].img}'>
+            <h4>${responseJson.meals[i].title}</h4>
+            <
+            
+
+        )
+    }
+}
+
+function getMealPlan(searchTerm, calories, diet, exclude){
+    const params ={
+        timeFrame: searchTerm,
+        targetCalories: calories,
+        diet: diet,
+        exclude: exclude
+    };
+
+    let queryString = $.param(params);
+    const url = planUrl + "?" + queryString;
+    console.log("url",url);
+
+    fetch(url).then(response =>{
+        if(response.ok){
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    }).then(responseJson => displayResults(responseJson))
+}
+
+function watchForm() {
+    $('form').subnit(event => {
+        event.preventDefault();
+        let searchTerm = $("#number-meals").val();
+        let calories = $("#calories").val();
+        let diet = $("#diet").val();
+        let exclude = $("#exclude").val();
+        console.log("data",searchTerm,calories,diet,exclude);
+        if(diet == ""){
+            diet=undefined;
+        }
+        if(exclude == ""){
+            exclude=undefined;
+        }
+        getMealPlan(searchTerm,calories,diet,exclude);
+    })
+}
+
+$(watchForm);
